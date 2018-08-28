e0bb075c7c3bd6bb096116f1ced11baa8c5afae0
SkyZH <iskyzh@gmail.com>
2018-08-28 11:45:41 +0800

    add temperature and voltage services

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import admin from './admin';
 import { Model, IReport } from '@skyzh/tick';
 import Service from './service';
 import * as _ from 'lodash';
-import * as NodeCache from 'node-cache'; 
+import * as NodeCache from 'node-cache';
 
 const db = admin.firestore();
 const raspiRef = db.collection('Mon_Raspi');
-const cpuRef = raspiRef.doc('CPU');
-const memRef = raspiRef.doc('Memory');
+
+const refs: { [id: string]: FirebaseFirestore.DocumentReference } = {
+  cpu: raspiRef.doc('CPU'),
+  memory: raspiRef.doc('Memory'),
+  voltage: raspiRef.doc('Voltage'),
+  temperature: raspiRef.doc('Temperature'),
+};
+
+const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
 
 function getPromise<T>(
   loc: FirebaseFirestore.DocumentReference,
@@ -27,42 +34,98 @@ function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function get_key(id: string): Promise<number> {
+  return new Promise((resolve, reject) => {
+    cache.get(id, (err, val) => {
+      if (err) {
+        reject(err);
+      } else {
+        if (!val) {
+          reject(null);
+        } else {
+          resolve(val as number);
+        }
+      }
+    });
+  });
+}
+
+function proxyGet(
+  name: string,
+  table: string,
+  id: string
+): Promise<number | null> {
+  return get_key(`${name}-${table}-${id}`).catch(err =>
+    refs[name]
+      .collection(table)
+      .doc(id)
+      .get()
+      .then(d => Promise.resolve(d.exists ? +d.data()!.data : null))
+  );
+}
+
+function proxyPut(name: string, report: IReport<number>): Promise<void> {
+  cache.set(`${name}-${report.table}-${report.id}`, report.data);
+  if (report.table !== 'second') {
+    return getPromise(refs[name], report).then(p => Promise.resolve());
+  } else {
+    return Promise.resolve();
+  }
+}
+
 async function main() {
   const cpuModel = new Model<number>(
     'cpu',
     (data: number[]) => _.mean(data),
-    (table: string, id: string) =>
-      cpuRef
-        .collection(table)
-        .doc(id)
-        .get()
-        .then(d => Promise.resolve(d.exists ? +d.data()!.data : null))
+    (table: string, id: string) => proxyGet('cpu', table, id)
   );
   const memModel = new Model<number>(
     'mem',
     (data: number[]) => _.mean(data) || 0,
-    (table: string, id: string) =>
-      memRef
-        .collection(table)
-        .doc(id)
-        .get()
-        .then(d => Promise.resolve(d.exists ? +d.data()!.data : null))
+    (table: string, id: string) => proxyGet('memory', table, id)
   );
-  const service = new Service(cpuModel, memModel);
+  const volModel = new Model<number>(
+    'voltage',
+    (data: number[]) => _.mean(data),
+    (table: string, id: string) => proxyGet('voltage', table, id)
+  );
+  const temModel = new Model<number>(
+    'temperature',
+    (data: number[]) => _.mean(data) || 0,
+    (table: string, id: string) => proxyGet('temperature', table, id)
+  );
+  const service = new Service(cpuModel, memModel, temModel, volModel);
   while (true) {
-    debug('Reporting Data...');
-    const { cpuReport, memReport } = await service.report();
-    debug('Saving Data...');
+    debug('Gathering Data');
+    const {
+      cpuReport,
+      memReport,
+      temReport,
+      volReport,
+    } = await service.report();
+    debug('Reporting Data');
     await Promise.all([
-      ..._.map(cpuReport, (report: IReport<number>) =>
-        getPromise(cpuRef, report)
-      ),
+      ..._.map(cpuReport, (report: IReport<number>) => proxyPut('cpu', report)),
       ..._.map(memReport, (report: IReport<number>) =>
-        getPromise(memRef, report)
+        proxyPut('memory', report)
+      ),
+      ..._.map(temReport, (report: IReport<number>) =>
+        proxyPut('temperature', report)
+      ),
+      ..._.map(volReport, (report: IReport<number>) =>
+        proxyPut('voltage', report)
       ),
     ]);
-    debug('Sleeping...');
-    await delay(3000);
+    debug(`Data at ${new Date(Date.now())} reported`);
+    debug(
+      [
+        `cpu: ${cpuReport[0].data}`,
+        `memory: ${memReport[0].data}`,
+        `temperature: ${temReport[0].data}`,
+        `voltage: ${volReport[0].data}`,
+      ].join('\n')
+    );
+    await delay(1000);
   }
 }
 
--- src/service.ts
+++ src/service.ts
@@ -3,15 +3,55 @@ import * as os from 'os';
 import { Model, IReport } from '@skyzh/tick';
 import * as _ from 'lodash';
 import * as moment from 'moment';
+import * as child_process from 'child_process';
+
+function getExec(
+  cmd: string,
+  arg: string[],
+  regex: RegExp
+): Promise<number | null> {
+  return new Promise((resolve, reject) => {
+    let result = '';
+    const child = child_process.execFile(
+      '/opt/vc/bin/vcgencmd',
+      ['measure_temp'],
+      (err, stdout, stderr) => {
+        result += stdout;
+      }
+    );
+    child.on('close', code => {
+      const mat = result.match(regex);
+      if (mat) {
+        resolve(+mat[1]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+}
+
+function getTemp(): Promise<number | null> {
+  return getExec('/opt/vc/bin/vcgencmd', ['measure_temp'], /temp=(.*)'C/);
+}
+
+function getVolt(): Promise<number | null> {
+  return getExec(
+    '/opt/vc/bin/vcgencmd',
+    ['measure_volts', 'core'],
+    /volt=(.*)V/
+  );
+}
 
 export default class Service {
   constructor(
     private cpuModel: Model<number>,
-    private memModel: Model<number>
+    private memModel: Model<number>,
+    private temModel: Model<number>,
+    private volModel: Model<number>
   ) {}
 
   public async report(): Promise<{ [name: string]: Array<IReport<number>> }> {
-    const [cpuReport, memReport] = await Promise.all([
+    const [cpuReport, memReport, temReport, volReport] = await Promise.all([
       this.cpuModel.report(
         _.mean(
           _.map(
@@ -24,10 +64,14 @@ export default class Service {
         moment(Date.now())
       ),
       this.memModel.report(+(os.totalmem() - os.freemem()), moment(Date.now())),
+      this.temModel.report((await getTemp()) || 0, moment(Date.now())),
+      this.volModel.report((await getVolt()) || 0, moment(Date.now())),
     ]);
     return {
       cpuReport,
       memReport,
+      temReport,
+      volReport,
     };
   }
 }
